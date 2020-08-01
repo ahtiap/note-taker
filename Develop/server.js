9a0089d6f98fa1ddf4d206c024a6dd95a2bc997e
Hachem Said <labsar.ahmed@outlook.fr>
2020-07-31 23:57:12 -0400

    added all routes and started on the id

--- Develop/server.js
+++ Develop/server.js
@@ -21,16 +21,13 @@ app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-//   * GET `*` - Should return the `index.html` file
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
-
 // * The application should have a `db.json` file on the backend that
 // will be used to store and retrieve notes using the`fs` module.
 
-var rawData = fs.readFileSync("./db/db.json");
+var rawData = fs.readFileSync("./db/db.json") || [];
 var notes = JSON.parse(rawData);
+
+var lastId = notes.length || 0;
 
 // * The following API routes should be created:
 
@@ -44,10 +41,14 @@ app.get("/api/notes", async (req, res) => {
 // new note to save on the request body, add it to the`db.json`
 // file, and then return the new note to the client.
 app.post("/api/notes", function (req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-
-  console.log(req.body);
+  var newNote = req.body;
+  res.json(newNote);
+  var lastId = notes.length || 0;
+  newNote.id = lastId;
+  console.log(newNote.id);
+  notes.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+  return newNote;
 });
 // * DELETE `/api/notes/:id` - Should receive a query parameter
 // containing the id of a note to delete.This means you'll need
@@ -55,6 +56,15 @@ app.post("/api/notes", function (req, res) {
 // In order to delete a note, you'll need to read all notes from
 // the`db.json` file, remove the note with the given`id` property,
 // and then rewrite the notes to the`db.json` file.
+app.delete("/api/notes/:id", function (req, res) {
+  res.send("DELETE request to homepage");
+});
+// * GET `*` - Should return the `index.html` file
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+////// listen function to start listening
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
+  console.log(notes);
 });
